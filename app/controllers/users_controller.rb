e77adcb5bf99d560483d1fbc507a347e499cd7e2
Gonzodamus <Dick@TheDickWard.com>
2018-01-11 13:31:10 -0500

    back end auth should be complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
     users = User.all
     render json: users
   end
-end
 
-def create
+
+  def create
     user = User.new(user_name: params[:user_name], password: params[:password])
     if user.save
       render json: {email: user.email, id: user.id, token: issue_token({id: user.id})}
     else
       render json: {error: "Invalid username or password"}, status: 401
     end
+  end
 end
