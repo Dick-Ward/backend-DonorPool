b89dcb9707b56d11379919bc8023743deabe3875
Gonzodamus <Dick@TheDickWard.com>
2018-01-12 13:59:23 -0500

    rendering username instead of email during api setup

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,7 +3,7 @@ class AuthController < ApplicationController
   def create
       user = User.find_by(user_name: params[:user_name])
       if user && user.authenticate(params[:password])
-        render json: {email: user.email, id: user.id, token: issue_token({id: user.id})}
+        render json: {user_name: user.user_name, id: user.id, token: issue_token({id: user.id})}
       else
         render json: {error: "Invalid username or password"}, status: 401
       end
@@ -15,7 +15,7 @@ class AuthController < ApplicationController
       id = decoded_token.first['id']
       user = User.find_by(id: id)
       if user
-        render json: {email: user.email, id: user.id, token: issue_token({id: user.id})}
+        render json: {user_name: user.user_name, id: user.id, token: issue_token({id: user.id})}
 
       else
         render json: {error: "Invalid token"}, status: 401
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_name: params[:user_name], password: params[:password])
     if user.save
-      render json: {email: user.email, id: user.id, token: issue_token({id: user.id})}
+      render json: {user_name: user.user_name, id: user.id, token: issue_token({id: user.id})}
     else
       render json: {error: "Invalid username or password"}, status: 401
     end
