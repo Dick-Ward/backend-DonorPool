8ec0da3aa0505612e7e6a1c438a1f551044a61de
Gonzodamus <Dick@TheDickWard.com>
2018-01-16 18:01:47 -0500

    fixed spelling in serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,13 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_name: params[:user_name], password: params[:password])
     if user.save
-      render json:  {token: issue_token({id: user.id}), data: UserSerializer.new(current_user)}
+      render json:  {token: issue_token({id: user.id}), data: UserSerializer.new(user)}
     else
       render json: {error: "Invalid username or password"}, status: 401
     end
   end
 
   def show
-    byebug
     user = User.find_by(id: params[:id])
     if user
       render json:  {token: issue_token({id: current_user.id}), data: UserSerializer.new(current_user)}
