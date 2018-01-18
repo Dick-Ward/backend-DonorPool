2385d4e3e945493c32da87345521003347959d32
Gonzodamus <Dick@TheDickWard.com>
2018-01-18 17:54:27 -0500

    moved create supports to supports controller

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,2 +1,11 @@
 class SupportsController < ApplicationController
+
+  def create
+    support = Support.new(user_id: params[:user_id], charity_id: params[:charity_id], donation: params[:amount] )
+    if support.save
+      render json: {message: "success"}, status: 200
+    else
+      render json: {error: "Unable to complete"}, status: 400
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   resources :users
   post '/auth', to: 'auth#create'
   get '/current_user', to: 'auth#show'
-  get '/featured_users', to: 'users#featured'
-  get '/my_updates/:id', to: 'updates#user_relevant_updates'
-  post '/users/add_support', to: 'users#add_supported_charity'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
