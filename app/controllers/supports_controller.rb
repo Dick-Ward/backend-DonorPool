ae4b169106be0eca2132042c4a8e23199830a5c5
Gonzodamus <Dick@TheDickWard.com>
2018-01-19 14:47:22 -0500

    updates now properly populate on support

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,9 +1,20 @@
 class SupportsController < ApplicationController
 
   def create
+
     support = Support.new(user_id: params[:user_id], charity_id: params[:charity_id], donation: params[:amount] )
     if support.save
-      render json: {message: "success"}, status: 200
+      updates = support.charity.updates
+      updateMap = updates.map{|update|
+      {content: update.content,
+              created_at: update.created_at,
+              id: update.id,
+              picture: update.picture,
+              title: update.title,
+              charity_name: update.charity.name,
+              charity_id: update.charity_id
+              }}
+      render json: {updates: updateMap}
     else
       render json: {error: "Unable to complete"}, status: 400
     end
