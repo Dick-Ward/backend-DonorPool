b26a1271b1bf0a11311e28e336cee9f293dc381e
Gonzodamus <Dick@TheDickWard.com>
2018-01-23 23:20:17 -0500

    update create method

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -5,15 +5,23 @@ class UpdatesController < ApplicationController
     render json: updates.map{|update| {title: update.title, content: update.content, picture: update.picture, charity_name: update.charity.name}}
   end
 
+  def create
+    update = Update.create(charity_id: params[:charityId], title: params[:title], content: params[:content], picture: "http://via.placeholder.com/700x400")
+    if update.save
+      render json: {message: "success"}
+    else
+      render json: {message: "naah"}
+    end
+  end
+
   def user_relevant_updates
     updates = Update.all
     if params[:id] != "undefined"
       render json: updates.select{|update| update.charity.supporters.map{|sport| sport.id}.include?(params[:id].to_i)}
-
     else
       render json: updates
-
     end
   end
+
 
 end
